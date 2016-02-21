39267e6a76e02f3f468f5f3e2ac9081cbf35ea3d
chishuliang <664165215@qq.com>
2016-02-22 04:36:47 +0800

    sc

--- js/json.js
+++ js/json.js
@@ -1,8 +1,9 @@
 var data = ["img/lunBo1.JPG", "img/lunBo2.JPG", "img/lunBo3.JPG"];
 var dataLi = ["img/lunBo1_1.JPG", "img/lunBo2_2.PNG", "img/lunBo3_3.JPG"];
-var sliderData = [{"bg": "img/lg1.jpg", "title": "喊你玩", "cent": "发现国外好住宿"},
-    {"bg": "img/lg2.jpg", "title": "七加二", "cent": "顺互联网之势,启智能户外之钧"},
-    {"bg": "img/lg3.jpg", "title": "再生活", "cent": "开放的氛围，快速成长的空间"},
-    {"bg": "img/lg4.jpg", "title": "壕鑫互联", "cent": "壕鑫互联"},
-    {"bg": "img/lg5.jpg", "title": "河马牙医", "cent": "口腔健康的移动平台"},
-    {"bg": "img/lg6.jpg", "title": "前海博信", "cent": "专注信用卡优惠的公司"}];
+var sliderData = [
+    {"bg": "img/CgYXBlUf3yKAENTpAAA1HMmBg1c125.jpg", "title": "乐舞", "cent": "专注广场舞，服务中老年社交"},
+    {"bg": "img/CgYXBlW4gleABVxwAAARtZOjFHY372.png", "title": "易易", "cent": "有梦想，有激情，有钱的创业团队"},
+    {"bg": "http://www.lagou.com/image1/M00/35/4B/Cgo8PFWbco2AH5UgAAAOu8TfMDs954.png", "title": "人人行", "cent": "九鼎控股旗下互联网金融业务板块，靠谱机会就等你来"},
+    {"bg": "img/CgYXBlTUWBWAUFOSAABYoBOBvGM636.jpg", "title": "销售易", "cent": "移动CRM领军企业"},
+    {"bg": "img/CgYXBlTUXXOADfRiAACyhNg0LgY628.png", "title": "元征科技", "cent": "车联网—有潜力"},
+    {"bg": "img/CgYXBlTUXHCAT83lAABvfj5-5FQ245.jpg", "title": "会小二网", "cent": "30分钟搞定会议场地"}];
